94bf305192bf3823a9f82b339b39d9a7bdb9495d
kaankoksal <kaankoksal23@outlook.com>
2021-04-24 14:52:33 +0200

    IMDB spider

---
+++ imdb_cast/imdb_cast/spiders/cast_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+from ..items import ImdbCastItem
+
+class CastSpider(scrapy.Spider):
+    name = 'cast_spider'
+    start_urls = ['https://www.imdb.com/chart/top/']
+
+    def parse(self, response):
+        # GET URLS FOR EACH MOVIE TITLE
+        #    extract the /title/.../ strings
+        urls = response.css("td.titleColumn a::attr(href)").extract() 
+        for url in urls:
+            # combine Response's url with possible relative url
+            request = scrapy.Request(response.urljoin(url), callback=self.parseCast) 
+            yield request
+        
+
+    def parseCast(self, response):
+        # SCRAPE CAST MEMBERS FROM EACH CAST LIST
+        cast_list = response.css("div#titleCast.article td a::text").extract()     
+        for member in cast_list:
+            cast = ImdbCastItem()
+            cast['name'] = str(member).strip()
+            yield cast
