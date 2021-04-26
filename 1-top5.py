2628be1f8b7e5817d98d248869fc04a14994d906
kaankoksal <kaankoksal23@outlook.com>
2021-04-26 22:16:27 +0200

    google drive upload

---
+++ 1-top5.py
@@ -0,0 +1,32 @@
+import csv
+import pandas as pd
+from collections import Counter
+
+
+data = []
+with open("imdb_cast/output.csv", encoding="utf-8", newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+# Initialize pandas dataframe to get value frequencies
+# and transfer data into python dict where keys are names 
+# and values are the frequency
+df = pd.DataFrame(data=data, columns=['name'])
+count = df.name.value_counts().to_dict()
+
+# Use counter to get top 5 
+k = Counter(count)
+five_highest = k.most_common(5)
+
+# Move top 5 actors/actresses to empty list 
+top5_cast_members = []
+for i in five_highest:
+    top5_cast_members.append(i[0])
+
+# Open the upload file and write the list content 
+upload_file = open('drive_upload/to_upload.csv','w')
+writer = csv.writer(upload_file, dialect='excel')
+
+for cast_member in top5_cast_members:
+    writer.writerow([cast_member])
+print("File write successful")
---
+++ 2-upload_csv.py
@@ -0,0 +1,41 @@
+import os
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
+
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+def export_csv(file_path: str, parents: list=None):
+    if not os.path.exists(file_path):
+        print(f"{file_path} not found")
+        return
+    try:
+        file_metadata = {
+            'name': os.path.basename(file_path).replace('.csv', " "),
+            'mimeType': "application/vnd.google-apps.spreadsheet",
+            'parents': parents
+        }
+
+        media = MediaFileUpload(filename=file_path, mimetype='text/csv')
+
+        response = service.files().create(
+            media_body =media,
+            body =file_metadata,
+        ).execute()
+
+        print(response)
+        return response
+    
+    except Exception as e:
+        print(e)
+        return
+
+csv_files = os.listdir("upload")
+
+for csv_file in csv_files:
+    export_csv(os.path.join('upload', csv_file))
