7f5884f99b3bda22abb32634e85c02cff85500b8
kaankoksal <kaankoksal23@outlook.com>
2021-04-29 20:46:59 +0200

    add classes

--- top5.py
+++ top5.py
@@ -1,32 +1,52 @@
 import csv
+import os
 import pandas as pd
 from collections import Counter
 
+class Top5():
+    def __init__(self):
+        pass
 
-data = []
-with open("imdb_cast/output.csv", encoding="utf-8", newline='') as f:
-    reader = csv.reader(f)
-    data = list(reader)
+    def get_top5(self):
 
-# Initialize pandas dataframe to get value frequencies
-# and transfer data into python dict where keys are names 
-# and values are the frequency
-df = pd.DataFrame(data=data, columns=['name'])
-count = df.name.value_counts().to_dict()
+        # Turn csv file content into list
+        data = []
 
-# Use counter to get top 5 
-k = Counter(count)
-five_highest = k.most_common(5)
+        try:
+            with open("imdb_cast/output.csv", encoding="utf-8", newline='') as f:
+                reader = csv.reader(f)
+                data = list(reader)
+        except OSError:
+            print("Could not read file\nPlease make sure output.csv is available")
 
-# Move top 5 actors/actresses to empty list 
-top5_cast_members = []
-for i in five_highest:
-    top5_cast_members.append(i[0])
+        # Initialize pandas dataframe to get value frequencies
+        # and transfer data into python dict where keys are names 
+        # and values are the frequency
+        df = pd.DataFrame(data=data, columns=['name'])
+        count = df.name.value_counts().to_dict()
 
-# Open the upload file and write the list content 
-upload_file = open('upload/to_upload.csv','w')
-writer = csv.writer(upload_file, dialect='excel')
+        # Use counter to get top 5 
+        k = Counter(count)
+        five_highest = k.most_common(5)
 
-for cast_member in top5_cast_members:
-    writer.writerow([cast_member])
-print(f"Top 5 cast members are: {top5_cast_members}\n...\nWriting into drive upload file now")
+        # Move top 5 actors/actresses to empty list 
+        top5_cast_members = []
+        for i in five_highest:
+            top5_cast_members.append(i[0])
+
+        # Open the upload file and write the list content 
+        upload_file = open('upload/to_upload.csv','w')
+        writer = csv.writer(upload_file, dialect='excel')
+
+        for cast_member in top5_cast_members:
+            writer.writerow([cast_member])
+        print(f"Top 5 cast members are: {top5_cast_members}\n...\nWriting into drive upload file now...")
+        # remove csv file after done to generate new one in next scrape run
+        os.remove("imdb_cast/output.csv")   
+        
+
+        
+
+    
+
+        
--- uploadToDrive.py
+++ uploadToDrive.py
@@ -2,44 +2,49 @@ import os
 from Google import Create_Service
 from googleapiclient.http import MediaFileUpload
 
-# CREATE SERVICE
-CLIENT_SECRET_FILE = 'client_secret.json'
-API_NAME = 'drive'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/drive']
+class Upload():
+    def __init__(self):
+        pass
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+    def driveUpload(self):
+        # CREATE SERVICE
+        CLIENT_SECRET_FILE = 'client_secret.json'
+        API_NAME = 'drive'
+        API_VERSION = 'v3'
+        SCOPES = ['https://www.googleapis.com/auth/drive']
 
-# UPLOAD CSV FILE TO GOOGLE DRIVE
-def export_csv(file_path: str, parents: list=None):
-    if not os.path.exists(file_path):
-        print(f"{file_path} not found")
-        return
-    try:
-        file_metadata = {
-            'name': os.path.basename(file_path).replace('.csv', " "),
-            'mimeType': "application/vnd.google-apps.spreadsheet",
-            'parents': parents
-        }
+        service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-        media = MediaFileUpload(filename=file_path, mimetype='text/csv')
+        # UPLOAD CSV FILE TO GOOGLE DRIVE
+        def export_csv(file_path: str, parents: list=None):
+            if not os.path.exists(file_path):
+                print(f"{file_path} not found")
+                return
+            try:
+                file_metadata = {
+                    'name': os.path.basename(file_path).replace('.csv', " "),
+                    'mimeType': "application/vnd.google-apps.spreadsheet",
+                    'parents': parents
+                }
 
-        response = service.files().create(
-            media_body =media,
-            body =file_metadata,
-        ).execute()
+                media = MediaFileUpload(filename=file_path, mimetype='text/csv')
 
-        print(response)
-        return response
-    
-    except Exception as e:
-        print(e)
-        return
+                response = service.files().create(
+                    media_body =media,
+                    body =file_metadata,
+                ).execute()
 
-# UPLOAD THE CSV FILES IN DIRECTORY
-csv_files = os.listdir("upload")
+                print(response)
+                return response
+            
+            except Exception as e:
+                print(e)
+                return
 
-for csv_file in csv_files:
-    print(f"uploading {csv_file} now...")
-    export_csv(os.path.join('upload', csv_file))
-print("drive upload completed")
+        # UPLOAD THE CSV FILES IN DIRECTORY
+        csv_files = os.listdir("upload")
+
+        for csv_file in csv_files:
+            print(f"uploading {csv_file} now...")
+            export_csv(os.path.join('upload', csv_file))
+        print("drive upload completed")
